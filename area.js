838124b812c632d2433089a1f205423241bb78f6
simonelorengo <mmail.simone@gmail.com>
2018-01-12 10:14:10 +0100

    Add area script in which there is the getArea function

---
+++ area.js
@@ -0,0 +1,14 @@
+function getArea(sides) {
+	if(sides.length === 2) {
+		if(sides[0] >= 0 && sides[1] >= 0) {
+			return sides[0] * sides[1];
+		}
+		else {
+			return -1;
+		}
+	}
+	else {
+		return -1;
+	}
+}
+
